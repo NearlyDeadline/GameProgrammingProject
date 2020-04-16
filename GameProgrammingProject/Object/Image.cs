4359e5e3cdd58ba3d38c9c618ff3d217dd5319b4
NearlyDeadline <1289790957@qq.com>
2020-04-16 17:49:20 +0800

    4.16挖坑

---
+++ GameProgrammingProject/Object/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProgrammingProject
+{
+    public class Image
+    {
+        public String FileAddress { get; set; }//图案的文件地址
+        public int Length { get; set; }//图案的长
+        public int Height { get; set; }//图案的高
+    }
+}
